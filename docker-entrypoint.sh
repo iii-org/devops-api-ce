c7ef5d7edd240f9f10dbe03b4a2a1bce49413fc1
Ivan <d1034182016@gmail.com>
2024-05-22 11:39:45 +0800

    Change gevent back to eventlet.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ export PYTHONPATH=/usr/src/app/apis
 
 # If arguments are passed to docker, run them instead
 if [ ! "$#" -gt 0 ]; then
-  gunicorn --worker-class gevent -w 1 'apis.api:start_prod()' --timeout 120  -b 0.0.0.0:10009
+  gunicorn --worker-class eventlet -w 1 'apis.api:start_prod()' --timeout 120 --threads 1 -b 0.0.0.0:10009
 fi
 
 exec "$@"
